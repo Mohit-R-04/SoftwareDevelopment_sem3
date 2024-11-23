822003a536010a7c365059b2460f649be908e2d1
Mohit-R-04 <mohit2310893@ssn.edu.in>
2024-11-23 08:35:28 +0530

    Initial commit

--- designPatternLab/ex-03/Q2/Complex.py
+++
@@ -1,8 +0,0 @@
-
-class Complex:
-    def __init__(self, real, imaginary):
-        self.real = real
-        self.imaginary = imaginary
-
-    def complex_distance(self, other):
-        return (((self.real - other.real) ** 2 + (self.imaginary - other.imaginary) ** 2) ** 0.5)
--- designPatternLab/ex-03/Q2/Number.py
+++
@@ -1,7 +0,0 @@
-
-class Number:
-    def __init__(self, value):
-        self.value = value
-
-    def number_distance(self, other):
-        return abs(self.value - other.value)
